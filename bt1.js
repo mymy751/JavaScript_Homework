4f71558784e624ab9b36ed709ef70ee83e9ac7f5
mymy751 <ntdmy751@gmail.com>
2023-04-08 20:54:36 +0700

    first init

---
+++ bt1.js
@@ -0,0 +1,12 @@
+// Cho 2 mang so khac nhau, tim cac so trong mang B ma khong co trong mang A
+// In ket qua duoi dang mang
+const arrayA = [1, 2, 3, 4, 5, 6, 7, 8];
+const arrayB = [1, 2, 5, 6, 7, 8];
+
+// Viet Logic trong day
+const main = () => {
+
+};
+
+// Expect output would be [3, 4]
+main();
---
+++ bt2.js
@@ -0,0 +1,11 @@
+/**
+ * Reverse a String
+ * Reverse the provided string and return the reversed string.
+ * For example, "hello" should become "olleh".
+ */
+
+function reverseString(str) {
+    return str;
+}
+
+reverseString("hello");
